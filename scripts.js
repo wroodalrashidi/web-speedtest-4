269f1f88acc190fa2576264eecedfd8d339ec4ae
ورود الرشيدي <66529914+wroodalrashidi@users.noreply.github.com>
2020-10-03 17:14:49 +0300

    done

--- scripts.js
+++ scripts.js
@@ -1,8 +1,17 @@
+
 let instructor = {
   name: "Menawer",
   courses: ["JavaScript", "React", "PHP"],
-  githubAccount: {
-    username: "careless10",
-    numberOfRepos: 200,
-  },
+     
+    githubAccount: {
+      username: "careless10",
+      numberOfRepos: 200,
+  },  
 };
+
+console.log(instructor.name)
+console.log(instructor.courses[0])
+console.log(instructor.githubAccount.username)
+
+type="text/javascript">
+alert("😎 استاذ مناور أقوى مدرس ويب");
